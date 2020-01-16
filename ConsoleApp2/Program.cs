2a7a308d421a54d1b996b18a5a78eae89a44ae44
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:41:57 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int komputery = 24;
+            int studenci;
+            double wynik;
+            Console.WriteLine("How many studends are there?");
+            studenci = Convert.ToInt32(Console.ReadLine());
+            wynik = (double) studenci / komputery;
+            Console.WriteLine($"There is {wynik} computers per student.");
+            Console.ReadKey();
+        }
+    }
+}
